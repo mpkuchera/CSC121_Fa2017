24db1ddd8e4304ceba0b2a34ef9122a65a1faee0
Michelle Kuchera <michelle.kuchera@gmail.com>
2017-08-25 08:36:30 -0400

    added comments

--- misc/pair_programming.py
+++ misc/pair_programming.py
@@ -20,24 +20,24 @@ print "there are", len(students), "students in the class. They are:"
 for student in students:
     print student, ",   "
 
-students_per_group = 2
+students_per_group = 2 # creating pairs
 
 num_groups = len(students) / students_per_group
 
-print("\n\nThere are",num_groups, "groups in the class.")
+print "\n\nThere are",num_groups, "groups in the class."
 
 np.random.shuffle(students)
 
+#Print each pair and their table assignment
 for i in range(0,int(num_groups)):
     print "Table ",i+1,": ",students[i*students_per_group],"and" ,students[i*students_per_group+1], " "
-  #  print(students[i*students_per_group],"and " ,students[i*students_per_group+1] )
 
+# if we have an extra student: add them to the last group
 if(len(students)%students_per_group != 0):
-    print("and" ,students[-1])
+    print "and" ,students[-1]
 
     
-    
-    
+# The below code manages the pair programming role timing    
 raw_input("\n\nPress `Enter` when ready to start pair programming:")
 inp_min = input('How much time do the students have in minutes? ')
 minutes = float(inp_min)
