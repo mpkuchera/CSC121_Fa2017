ed6f7bd32cd12534d6891a2d667c3090bca2148c
Michelle Kuchera <mikuchera@davidson.edu>
2017-08-29 21:47:53 -0400

    chumbawamba lyrics, brute force

---
+++ lectures/Lecture4/chumbawamba.py
@@ -0,0 +1,90 @@
+
+"""
+Created Aug 27 2017, Edited August 28, 2017 to remove last chorus
+
+@author: Michelle P. Kuchera
+@brief: This program prints out the lyrics to Chumawamba's hit song \
+        Tubthumping.
+"""
+
+
+print "We'll be singing"
+print "When we're winning"
+print "We'll be singing"
+print 
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print 
+print "Pissing the night away"
+print "Pissing the night away"
+print 
+print "He drinks a whiskey drink"
+print "He drinks a vodka drink"
+print "He drinks a lager drink"
+print "He drinks a cider drink"
+print "He sings the songs that remind him of the good times"
+print "He sings the songs that remind him of the better times"
+print 
+print "Oh, Danny Boy"
+print "Danny Boy"
+print "Danny Boy"
+print 
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print 
+print "Pissing the night away"
+print "Pissing the night away"
+print 
+print "He drinks a whiskey drink"
+print "He drinks a vodka drink"
+print "He drinks a lager drink"
+print "He drinks a cider drink"
+print "He sings the songs that remind him of the good times"
+print "He sings the songs that remind him of the better times"
+print 
+print "Don't cry for me"
+print "Next door neighbor"
+print 
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+print "I get knocked down"
+print "But I get up again"
+print "You're never gonna keep me down"
+#print 
+#print "I get knocked down"
+#print "But I get up again"
+#print "(We'll be singing)"
+#print "You're never going to keep me down"
+#print "(When we're winning)"
+#print "I get knocked down"
+#print "But I get up again"
+#print "(We'll be singing)"
+#print "You're never going to keep me down"
+
